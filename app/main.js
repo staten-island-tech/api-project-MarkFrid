fa717b5e3ff9c1a983a5d54ec63a4b042a7ea2d8
MarkFrid <mark.fridlin@gmail.com>
2024-11-18 12:32:20 -0500

    ss

--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+//api!!!!
+async function getData() {
+  //fetch returns promise
+  try {
+    const response = await fetch("https://api.chess.com/pub/leaderboards");
+    //gaurd clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textcontent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("mb dont know where pokemon");
+  }
+}
+
+getData();
