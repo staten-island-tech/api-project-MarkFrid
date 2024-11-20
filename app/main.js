21d3da30e1334578b9b40c091f034140c0ab123c
MarkFrid <mark.fridlin@gmail.com>
2024-11-20 12:30:51 -0500

    s

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ async function getData(URL) {
     const data = await response.json();
     console.log(data.live_blitz[0].name);
     run(data);
+    displayCards(data);
   } catch (error) {
     console.log(error);
   }
@@ -20,7 +21,32 @@ const DOMSelectors = {
 };
 function run(data) {
   DOMSelectors.live_blitz.addEventListener("click", () => {
-    console.log(data.live_blitz[0].name);
+    for (let i = 0; i < 50; i++) {
+      console.log(data.live_blitz[i].name);
+    }
   });
 }
 getData(URL);
+
+function displayCards(data) {
+  DOMSelectors.container.innerHTML = "";
+
+  data.forEach((card) => {
+    idCounter += 1;
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card" id="card-${idCounter}">
+        <h2 class="card-heading" id="heading-${idCounter}">${card.name}</h2>
+        <h3 class="card-subheading" id="subheading-${idCounter}">user: ${
+        card.username
+      }</h3>
+        <h3 class="card-rating">Price: $${card.rating.toFixed(2)}</h3>
+        ${
+          card.imageUrl
+            ? `<img class="card-img" id="img-${idCounter}" src="${card.imageUrl}" alt="${card.altText}">`
+            : ""
+        }
+      </div>`
+    );
+  });
+}
